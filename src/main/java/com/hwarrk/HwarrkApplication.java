ec23bd867afba0872dcb402ffbe59a83e10c2136
LSH <lsh2613@naver.com>
2024-08-13 22:41:51 +0900

    [feat] 초기 세팅

---
+++ src/main/java/com/hwarrk/HwarrkApplication.java
@@ -0,0 +1,13 @@
+package com.hwarrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwarrkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwarrkApplication.class, args);
+    }
+
+}
