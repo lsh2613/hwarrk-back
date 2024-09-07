9b28a4fcccfe94b24eab409ac641ba97a7d75357
LSH <lsh2613@naver.com>
2024-09-07 16:57:58 +0900

    [mod] 사용하지 않는 @slf4j 제거

--- src/main/java/com/hwarrk/domain/project/controller/ProjectController.java
+++ src/main/java/com/hwarrk/domain/project/controller/ProjectController.java
@@ -7,13 +7,11 @@ import com.hwarrk.domain.project.dto.res.ProjectRes;
 import com.hwarrk.domain.project.service.ProjectService;
 import com.hwarrk.global.common.apiPayload.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/projects")
