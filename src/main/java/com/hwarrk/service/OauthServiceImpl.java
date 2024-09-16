51ab20aa3ff587e815ac65ed290fe9468a226b34
LSH <lsh2613@naver.com>
2024-09-17 01:27:21 +0900

    [fix] 기존 유저 조회 시 발생하는 버그 해결

--- src/main/java/com/hwarrk/service/OauthServiceImpl.java
+++ src/main/java/com/hwarrk/service/OauthServiceImpl.java
@@ -1,12 +1,11 @@
 package com.hwarrk.service;
 
-import com.hwarrk.common.constant.Role;
-import com.hwarrk.jwt.TokenProvider;
-import com.hwarrk.entity.Member;
-import com.hwarrk.repository.MemberRepository;
 import com.hwarrk.common.dto.res.OauthLoginRes;
+import com.hwarrk.entity.Member;
+import com.hwarrk.jwt.TokenProvider;
 import com.hwarrk.oauth2.member.OauthMember;
 import com.hwarrk.oauth2.param.OauthParams;
+import com.hwarrk.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,16 +17,9 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class OauthServiceImpl implements OauthService{
-    /**
-     * Service
-     */
+public class OauthServiceImpl implements OauthService {
     private final RequestOauthInfoService requestOauthInfoService;
     private final TokenProvider tokenProvider;
-
-    /**
-     * repository
-     */
     private final MemberRepository memberRepository;
 
     @Override
@@ -37,20 +29,17 @@ public class OauthServiceImpl implements OauthService{
         OauthMember request = requestOauthInfoService.request(oauthParams);
         Optional<Member> byOauthProviderAndSocialId = memberRepository.findByOauthProviderAndSocialId(request.getOauthProvider(), request.getSocialId());
 
+        // 기존 유저
         if (byOauthProviderAndSocialId.isPresent()) {
-            //기존 유저
-            if (byOauthProviderAndSocialId.get().getRole().equals(Role.USER))
-                return new OauthLoginRes(
-                        byOauthProviderAndSocialId.get().getId(),
-                        byOauthProviderAndSocialId.get().getRole(),
-                        tokenProvider.issueAccessToken(byOauthProviderAndSocialId.get().getId()),
-                        tokenProvider.issueRefreshToken(byOauthProviderAndSocialId.get().getId()));
-            //신규 게스트
-            else memberRepository.deleteById(byOauthProviderAndSocialId.get().getId());
+            return new OauthLoginRes(
+                    byOauthProviderAndSocialId.get().getId(),
+                    byOauthProviderAndSocialId.get().getRole(),
+                    tokenProvider.issueAccessToken(byOauthProviderAndSocialId.get().getId()),
+                    tokenProvider.issueRefreshToken(byOauthProviderAndSocialId.get().getId()));
         }
 
         //신규 유저 DB에 저장
         Member savedMember = memberRepository.save(new Member(request));
-        return new OauthLoginRes(savedMember.getId(), savedMember.getRole(), null,null);
+        return new OauthLoginRes(savedMember.getId(), savedMember.getRole(), null, null);
     }
 }
