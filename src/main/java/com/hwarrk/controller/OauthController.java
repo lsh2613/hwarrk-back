16b0ce9616f0188c79d7670f4e21d83d28a5fe82
LSH <lsh2613@naver.com>
2024-09-17 01:29:33 +0900

    [docs] OauthController에 api 설명 어노테이션 추가

--- src/main/java/com/hwarrk/controller/OauthController.java
+++ src/main/java/com/hwarrk/controller/OauthController.java
@@ -8,6 +8,10 @@ import com.hwarrk.oauth2.param.GoogleParams;
 import com.hwarrk.oauth2.param.KakaoParams;
 import com.hwarrk.oauth2.param.OauthParams;
 import com.hwarrk.service.OauthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +19,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "소셜 로그인")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/oauth2")
 public class OauthController {
     private final OauthService oauthService;
 
+    @Operation(summary = "소셜 로그인", description = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=47fe7aff98cf882892387fe4b65d3279&redirect_uri={redirectURL}을 통해 code값 받아오기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "COMMON400", description = "처리할 수 없는 소셜 로그인")}
+    )
     @PostMapping("/login")
     public CustomApiResponse<OauthLoginRes> socialLogin(@Valid @RequestBody OauthLoginReq oauthLoginReq) {
         OauthParams oauthParam;
