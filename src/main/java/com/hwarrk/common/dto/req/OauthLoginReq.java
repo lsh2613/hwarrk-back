acbf22a8a00a22f40e57fcd894a68581337c476e
LSH <lsh2613@naver.com>
2024-09-17 01:29:33 +0900

    [docs] OauthController에 api 설명 어노테이션 추가

--- src/main/java/com/hwarrk/common/dto/req/OauthLoginReq.java
+++ src/main/java/com/hwarrk/common/dto/req/OauthLoginReq.java
@@ -1,6 +1,7 @@
 package com.hwarrk.common.dto.req;
 
 import com.hwarrk.common.constant.OauthProvider;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class OauthLoginReq {
     @NotNull
+    @Schema(defaultValue = "KAKAO")
     private OauthProvider oauthProvider;
     @NotBlank
     private String code;
