b0b3c78b703002c74e940789efe50cbbdf31a19c
LSH <lsh2613@naver.com>
2024-09-27 15:55:52 +0900

    [mod] RESTFul하게 mapping URL 수정

--- src/main/java/com/hwarrk/controller/MemberLikeController.java
+++ src/main/java/com/hwarrk/controller/MemberLikeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/memberLikes")
+@RequestMapping("/member-likes")
 public class MemberLikeController {
 
     private final MemberLikeService memberLikeService;
