6f076dd916b409b904c0b56eaae9a669f4465ed4
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-10-13 17:03:45 +0900

    [feat] post-post 생성 기능 구현

--- src/main/java/com/hwarrk/service/PostService.java
+++ src/main/java/com/hwarrk/service/PostService.java
@@ -4,6 +4,7 @@ import com.hwarrk.common.EntityFacade;
 import com.hwarrk.common.dto.req.PostCreateReq;
 import com.hwarrk.common.dto.req.PostCreateReq.RecruitingPositionReq;
 import com.hwarrk.entity.Post;
+import com.hwarrk.entity.Project;
 import com.hwarrk.entity.RecruitingPosition;
 import com.hwarrk.repository.PostRepository;
 import java.util.Optional;
@@ -20,12 +21,14 @@ public class PostService {
     private final PostRepository postRepository;
 
     public Long createPost(PostCreateReq req) {
-        if (Optional.ofNullable(postRepository.findByProject(entityFacade.getProject(req.getProjectId())))
+        Project project = entityFacade.getProject(req.getProjectId());
+        if (Optional.ofNullable(postRepository.findByProject(project))
                 .isPresent()) {
             throw new IllegalStateException("프로젝트에 이미 공고가 존재합니다.");
         }
 
         Post post = req.createPost();
+        post.addProject(project);
         post.addSkills(req.getSkills());
 
         for (RecruitingPositionReq positionReq : req.getRecruitingPositionReqList()) {
