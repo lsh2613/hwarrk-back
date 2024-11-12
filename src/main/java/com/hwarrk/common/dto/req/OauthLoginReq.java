1ad479b807bad28e4d4cc811689a55c3b97a144c
LSH <lsh2613@naver.com>
2024-11-13 00:02:48 +0900

    [docs] oauth login 파라미터 설명 추가

--- src/main/java/com/hwarrk/common/dto/req/OauthLoginReq.java
+++ src/main/java/com/hwarrk/common/dto/req/OauthLoginReq.java
@@ -13,8 +13,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class OauthLoginReq {
     @NotNull
-    @Schema(defaultValue = "KAKAO")
+    @Schema(name = "소셜 로그인 제공자", examples = {"KAKAO", "GOOGLE", "APPLE"})
     private OauthProvider oauthProvider;
+
     @NotBlank
+    @Schema(description = "OAuth Provider Server로 부터 받은 인증 코드")
     private String code;
 }
